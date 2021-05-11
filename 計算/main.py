c44c28f5bd31ddbb3991ae5e6792e6a5c8f6f020
kitty88825 <kitty40632@gmail.com>
2021-05-11 14:24:13 +0800

    修正 計算

--- 計算/main.py
+++ 計算/main.py
@@ -8,22 +8,23 @@ def count(n, k):
     if k == 0:
         return 1
 
-    dp = [['' for i in range(12)] for j in range(98)]
-    # print(dp)
     if dp[n][k] != -1:
         return dp[n][k]
 
     val = 0
     i = 0
     while i < n and k-i >= 0:
-        i += 1
         val += count(n-1, k-i)
+        i += 1
 
     dp[n][k] = val
+
     return dp[n][k]
 
 
 # 第一筆為 input 數量
 for line in sys.stdin.read().splitlines()[1::]:
     n, k = line.split(' ')
+    # dp 要放這不然會 timeout
+    dp = [[-1 for i in range(99)] for j in range(13)]
     print(count(int(n), int(k)))
